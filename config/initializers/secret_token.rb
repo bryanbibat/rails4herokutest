a7dd992a8c38acc355358a43a81fb8c35e0a411f
Bryan Bibat <bry@bryanbibat.net>
2013-07-04 16:49:17 +0800

    Create Heroku test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = '23adfc60d9735455217d31c305010e7b8d9949eb38be6b4a20ddfe2a78de404f1bf3cad73ec1b4455d5e0213292e0284253e645a780954e9e97a4b35c74f56f4'
